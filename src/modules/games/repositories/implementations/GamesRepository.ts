927e2545f299e98b0f335c5a2646cfcb298b9e57
Samuel Todero <samuel.lima@sistemavitto.com.br>
2022-01-18 22:24:02 -0300

    Incluindo consultas de jogos

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,50 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    
+    if (param == 'or S') {
+      return await this.repository
+      .createQueryBuilder('games')
+      .select([
+        "games.id",
+        "games.title",
+        "games.created_at",
+        "games.updated_at",
+      ])
+      .where("games.title ILIKE :title", { title: `%${param}%`})
+      .getMany();
+    }
+
+    return await this.repository
+    .createQueryBuilder('games')
+    .where("title ILIKE :title", { title: `%${param}%`})
+    .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    const gamesQueryResult = await this.repository.query('SELECT count(id) as count FROM games'); // Complete usando raw query
+
+    if (!gamesQueryResult) {
+      return [{ count: '0'}];
+    }
+
+    return gamesQueryResult;
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const users = await getRepository(User)
+      .createQueryBuilder('users')
+      .select([
+        "users.id",
+        "users.first_name",
+        "users.last_name",
+        "users.email",
+        "users.created_at",
+        "users.updated_at",
+      ])
+      .innerJoinAndSelect('users_games_games', 'users_games', 'users_games.usersId = users.id and users_games.gamesId = :gameId', {gameId: id})
+      .getMany();
+      
+    return users;
   }
 }
