cf9a6d29944ca870802517d5eea34d6e579a0f30
Samuel Todero <samuel.lima@sistemavitto.com.br>
2022-01-18 22:23:40 -0300

    Incluindo consultas de usuários

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,25 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    
+    const users = await this.repository.find({
+      where: {
+        id: user_id,
+      },
+      relations: ['games']
+    })
+    
+    return users[0];
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query('SELECT * FROM users WHERE LOWER(first_name) = $1 and LOWER(last_name) = $2', [first_name.toLocaleLowerCase(), last_name.toLocaleLowerCase()]);
   }
 }
